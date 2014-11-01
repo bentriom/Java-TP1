7b63dca42b34f015ef7a7f3575d59436f84649cf
bentriom <mahmoud.bentriou@ensimag.grenoble-inp.fr>
2014-11-01 14:19:00 +0100

    Ajout classe donneessimulation non testee (manque classe robot et incendie)

---
+++ src/DonneesSimulation.java
@@ -0,0 +1,56 @@
+/**
+ * Classe qui contient toutes les informations de la simulation : cartes, robots, incendies
+ * Le constructeur prend en argument le nombre de lignes, colonnes et la taille des cases
+ * @author Mahmoud Bentriou, Mathias Biehler, Cyril Dutrieux
+ *
+ */
+
+import java.util.LinkedList;
+
+public class DonneesSimulation {
+	
+	/* Données */
+	private LinkedList<Robot> robots;
+	private LinkedList<Incendie> incendies;
+	private Carte carte;
+	
+	/* Constructeur, pas d'utilité de faire un constructeur de copie ici */
+	public DonneesSimulation(int lignes, int colonnes, int taillecase) {
+		/* Initialisation des listes */
+		incendies = new LinkedList<Incendie>();
+		robots = new LinkedList<Robot>();
+		/* Création de la carte */
+		this.carte = new Carte(lignes,colonnes,taillecase);
+	}
+	
+	/* Méthodes pour la carte : un seul accesseur, on utilise ensuite les méthodes de Carte */	
+	public Carte getCarte() {
+		return this.carte;
+	}
+	
+	/* Méthodes pour la liste de robots */
+	public int getSizeRobot() {
+		return this.robots.size();
+	}
+	
+	public Robot getRobot(int numRobot) {
+		return this.robots[numRobot];
+	}
+	
+	public void ajouteRobot(Robot r) {
+		this.robots.add(r);
+	}
+	
+	/* Méthode pour la liste d'incendies */
+	public int getSizeIncendie() {
+		return this.incendies.size();
+	}
+	
+	public Robot getIncendie(int numIncendie) {
+		return this.incendies[numIncendie];
+	}
+	
+	public void ajouteIncendie(Incendie i) {
+		this.incendies.add(i);
+	}	
+}
