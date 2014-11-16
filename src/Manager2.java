95e02c024451b86bfa1f0f556a25b46445966a5a
Dutrieux <cyril.dutrieux@ensimag.imag.fr>
2014-11-16 16:19:02 +0100

    fezfkzf2

---
+++ src/Manager2.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+
+
+public class Manager2 extends Manager {
+	private int clock = 0;
+	
+	public Manager2(Simulateur s) {
+		super(s);
+	}
+
+	@Override
+	public void manage() {
+		clock++;
+		if (clock % 10 != 0){
+			return;
+		}
+		LinkedList<Evenement> moveEvents;
+		DonneesSimulation data = simu.getData();
+		long dateCour = simu.getDate();
+		int nbIncendies = data.getSizeIncendie();
+		int nbRobots = data.getSizeRobot();
+		for (int i=0; i<nbIncendies; i++){
+			Incendie fire= simu.getData().getIncendie(i);
+			double coutMin = Double.MAX_VALUE;
+			Robot nextest = null;
+			for (int r=0; r<nbRobots; r++){
+				Robot robot = simu.getData().getRobot(r);
+				if (robot.isBusy()) {
+					continue;
+				}
+				if (robot.getWaterVol() <= 0) {
+					robot.fetchWater();
+					robot.remplirEau(dateCour);
+				}
+				double coutLoc = robot.timeToMoveTo(fire.getPosition());
+				if ( coutLoc <= coutMin && coutLoc >= 0){
+					nextest = robot;
+					coutMin = coutLoc;
+				}
+			}
+			// ajout_evt : beginning derverser eau, date = coutMin
+			moveEvents = moveEvents.add(nextest.moveToFar(fire.getPosition()));
+			// ajout evt : fin de derversage, date = coutMin + watertime
+			 nbIncendies = data.getSizeIncendie();
+		}
+	}
+
+}
---
+++ src/Manager3.java
@@ -0,0 +1,59 @@
+import java.util.LinkedList;
+
+
+public class Manager3 extends Manager {
+	private int clock = 0;
+	
+	public Manager3(Simulateur s) {
+		super(s);
+	}
+
+	@Override
+	public void manage() {
+		clock++;
+		if (clock % 10 != 0){
+			return;
+		}
+		DonneesSimulation data = simu.getData();
+		int nbIncendies = data.getSizeIncendie();
+		int nbRobots = data.getSizeRobot();
+		LinkedList<Evenement> moveEvents;
+		
+		int[] vivaciteFeu = new int[nbIncendies];
+		for (int i=0; i<nbIncendies; i++){
+			Incendie fire= simu.getData().getIncendie(i);
+			vivaciteFeu[i] = fire.getWaterNeed();
+		}
+
+		for (int r=0; r<nbRobots; r++){
+			Robot robot = simu.getData().getRobot(r);
+			if (robot.isBusy()) {
+				continue;
+			}
+			if (robot.getWaterVol() <= 0) {
+				robot.fetchWater();
+			}
+			double coutMin = Double.MAX_VALUE;
+			Incendie nextest = null;
+			int intNextest = 0;
+			for (int i=0; i<nbIncendies; i++){
+				Incendie fire= simu.getData().getIncendie(i);
+				if (vivaciteFeu[i] <= 0){
+					continue;
+				}
+				double coutLoc = robot.timeToMoveTo(fire.getPosition());
+				if ( coutLoc <= coutMin && coutLoc >= 0){
+					nextest = fire;
+					coutMin = coutLoc;
+					intNextest = i;
+				}
+			}
+			
+			vivaciteFeu[intNextest] -= robot.getWaterVol();
+			// ajout_evt : beginning derverser eau, date = coutMin
+			moveEvents = robot.moveToFar(nextest.getPosition());
+			// ajout evt : fin de derversage, date = coutMin + watertime
+		}
+	}
+
+}
