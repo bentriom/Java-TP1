c86366db41f10d97eebef4501891787bd7b3cb9c
bentriom <mahmoud.bentriou@ensimag.grenoble-inp.fr>
2014-11-19 18:12:57 +0100

    add exceptions

---
+++ src/ExceptionTeleportation.java
@@ -0,0 +1,5 @@
+public class ExceptionTeleportation extends Exception {
+    public ExceptionTeleportation() {
+        System.out.println("Le robot essaye de se deplacer sur une case qui n'est pas a coté. ");
+    }
+}
